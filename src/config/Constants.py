197cd34fbc9d7afcaf2ab355fb37181f7fa834ef
Vaibhou Pandey <dresta.personal@gmail.com>
2024-05-24 18:49:00 +0530

    feat : Added Colors and Dimension Consistency in Config.

---
+++ src/config/Constants.py
@@ -0,0 +1,18 @@
+class Colors:
+    # Snake Element Colors
+    SNAKE = (255, 255, 255)  # White
+    SNAKE_HEAD = (0, 0, 255)  # Bright Blue
+    FRUIT = (255, 0, 0)  # Bright Red
+    PATH = (255, 255, 0, 50)  # Yellow with transparency
+
+
+    # Screen Colors
+    WINDOW_BG = (108, 187, 60)
+    BANNER_COLOR = (189, 189, 189)
+
+
+class Dimensions:
+    # Screen Size
+    CELL_SIZE = 40
+    NO_OF_CELLS = 20
+    BANNER_HEIGHT = 2
