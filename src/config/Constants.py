1f1a74f706a37b21154a97a1012ee9b0f6e67390
Vaibhou Pandey <dresta.personal@gmail.com>
2024-05-29 11:19:50 +0530

    fix: Faced issue to define classes in GUI, so removed imported all instead.

--- src/config/Constants.py
+++ src/config/Constants.py
@@ -1,18 +1,36 @@
-class Colors:
-    # Snake Element Colors
-    SNAKE = (255, 255, 255)  # White
-    SNAKE_HEAD = (0, 0, 255)  # Bright Blue
-    FRUIT = (255, 0, 0)  # Bright Red
-    PATH = (255, 255, 0, 50)  # Yellow with transparency
+# screen dimensions
+CELL_SIZE = 40
+NO_OF_CELLS = 20
+BANNER_HEIGHT = 2
 
+# button + text field dimensions
+BTN_WIDTH = 200
+BTN_HEIGHT = 80
+TXT_WIDTH = 150
+TXT_HEIGHT = 30
 
-    # Screen Colors
-    WINDOW_BG = (108, 187, 60)
-    BANNER_COLOR = (189, 189, 189)
+# seed
+USER_SEED = 76767
 
+# COLORS
+# 1) buttons
+BTN_COLOR = (118, 131, 163)
+BTN_HOVER = (94, 219, 111)
+BTN_CLICKED = (94, 219, 111)
 
-class Dimensions:
-    # Screen Size
-    CELL_SIZE = 40
-    NO_OF_CELLS = 20
-    BANNER_HEIGHT = 2
+# 2) text field
+TXT_PASSIVE = (162, 163, 163)
+TXT_ACTIVE = (94, 219, 111)
+
+# 3) snake + path + fruit
+SNAKE_COLOR = (235, 235, 235)
+SNAKE_HEAD_COLOR = (106, 164, 189)
+FRUIT_COLOR = (219, 90, 101)
+PATHCOLOR = (41, 255, 3, 50)
+
+# 4) related to screen
+WINDOW_COLOR = (40, 40, 41)
+WHITE = (241, 241, 241)
+MENU_COLOR = (94, 219, 111)
+BANNER_COLOR = (189, 189, 189)
+TITLE_COLOR = (133, 209, 242)
